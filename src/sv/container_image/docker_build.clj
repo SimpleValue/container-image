758c49632387b0038f870ebcf8c04d9b44816433
Max Weber <weber.maximilian@gmail.com>
2021-07-11 17:02:30 +0200

    Added Docker build utils.

---
+++ src/sv/container_image/docker_build.clj
@@ -0,0 +1,20 @@
+(ns sv.container-image.docker-build
+  (:require [babashka.process :as process]))
+
+(defn new-tag
+  []
+  (.format (java.time.format.DateTimeFormatter/ofPattern
+            "yyyyMMddHHmmss")
+           (java.time.LocalDateTime/now)))
+
+(defn build!
+  [{:keys [image target]}]
+  (let [image-tag (str image
+                       ":"
+                       (new-tag))]
+    @(process/process "docker" "build" "--target" target "-t" image-tag ".")
+    (spit (str target
+               "-latest")
+          image-tag)
+    @(process/process "docker" "push" image-tag)
+    ))
