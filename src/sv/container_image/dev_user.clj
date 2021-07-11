42a35d1e9bdf87e8ab56522824179104f5971262
Max Weber <weber.maximilian@gmail.com>
2021-07-11 16:16:06 +0200

    Fix: ownership of `/app` folder

--- src/sv/container_image/dev_user.clj
+++ src/sv/container_image/dev_user.clj
@@ -5,5 +5,12 @@
   [["useradd" "-ms" "/bin/bash" "dev"]
    ["usermod" "-aG" "sudo" "dev"]
    ["apt-get" "install" "-y" "sudo"]
-   ["bash" "-c" "echo 'dev    ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"]]
+   ["bash" "-c" "echo 'dev    ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"]
+
+   ;; The `/app` folder (with the main application) should be owned by
+   ;; the dev user, so that it is allowed to create and modify files
+   ;; in this folder:
+   ["mkdir" "-p" "/app"]
+   ["chown" "dev" "/app"]
+   ]
   )
