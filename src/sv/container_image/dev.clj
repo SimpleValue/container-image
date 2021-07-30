dbfa15d4fe86dc6d56eceb5ef5f38946e4b28a0e
Max Weber <weber.maximilian@gmail.com>
2021-07-30 08:00:49 +0200

    Added Terraform to the dev-image.

--- src/sv/container_image/dev.clj
+++ src/sv/container_image/dev.clj
@@ -5,12 +5,14 @@
             [sv.container-image.datomic :as datomic]
             [sv.container-image.process :as process]
             [sv.container-image.kubectl :as kubectl]
+            [sv.container-image.terraform :as terraform]
             ))
 
 (defn install
   [params]
   (concat
    (kubectl/install params)
+   (terraform/install params)
    (yarn/install params)
    (gcloud/install params)
    (node/install params)
