559d45106c361e1fabff8d8e16d78bee9c76e11c
Max Weber <weber.maximilian@gmail.com>
2021-09-30 08:04:04 +0200

    Added install script for OpenVSCode Server

---
+++ src/sv/container_image/openvscode.clj
@@ -0,0 +1,25 @@
+(ns sv.container-image.openvscode
+  "Installs [OpenVSCode Server](https://github.com/gitpod-io/openvscode-server/)")
+
+(defn install
+  [params]
+  (let [version (::version params
+                           "1.60.2")]
+    [
+     ["wget"
+      (str "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v"
+           version
+           "/openvscode-server-v"
+           version
+           "-linux-x64.tar.gz")
+      "-O" "/code-server.tar.gz"]
+     ["tar" "-xzf" "/code-server.tar.gz"]
+     ["rm" "code-server.tar.gz"]
+     ["mv"
+      (str "/code-server/openvscode-server-v"
+           version
+           "-linux-x64")
+      "/opt/openvscode"]
+     ["chown" "-R" "dev" "/opt/openvscode"]
+     ]
+    ))
