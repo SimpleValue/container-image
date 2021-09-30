ec1e4117253e82d745300fbffa62808a4b5233fc
Max Weber <weber.maximilian@gmail.com>
2021-09-30 09:20:07 +0200

    Fix: wrong paths

--- src/sv/container_image/openvscode.clj
+++ src/sv/container_image/openvscode.clj
@@ -14,9 +14,9 @@
            "-linux-x64.tar.gz")
       "-O" "/code-server.tar.gz"]
      ["tar" "-xzf" "/code-server.tar.gz"]
-     ["rm" "code-server.tar.gz"]
+     ["rm" "/code-server.tar.gz"]
      ["mv"
-      (str "/code-server/openvscode-server-v"
+      (str "/openvscode-server-v"
            version
            "-linux-x64")
       "/opt/openvscode"]
