7ce5f2c2153e10a04b3519e68d3fcd1c746286f3
Max Weber <weber.maximilian@gmail.com>
2021-09-30 09:22:23 +0200

    Made paths relative

--- src/sv/container_image/openvscode.clj
+++ src/sv/container_image/openvscode.clj
@@ -12,11 +12,11 @@
            "/openvscode-server-v"
            version
            "-linux-x64.tar.gz")
-      "-O" "/code-server.tar.gz"]
-     ["tar" "-xzf" "/code-server.tar.gz"]
-     ["rm" "/code-server.tar.gz"]
+      "-O" "code-server.tar.gz"]
+     ["tar" "-xzf" "code-server.tar.gz"]
+     ["rm" "code-server.tar.gz"]
      ["mv"
-      (str "/openvscode-server-v"
+      (str "openvscode-server-v"
            version
            "-linux-x64")
       "/opt/openvscode"]
