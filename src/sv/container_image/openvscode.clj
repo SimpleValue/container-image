29b84876e72eb1257b3a04460b55496934b32aea
Max Weber <weber.maximilian@gmail.com>
2021-10-13 09:19:03 +0200

    Fix: max_user_watches cannot be adapted during the Docker image build.

--- src/sv/container_image/openvscode.clj
+++ src/sv/container_image/openvscode.clj
@@ -1,36 +1,34 @@
 (ns sv.container-image.openvscode
-  "Installs [OpenVSCode Server](https://github.com/gitpod-io/openvscode-server/)")
+  "Installs [OpenVSCode Server](https://github.com/gitpod-io/openvscode-server/)
+
+   To allow Visual Studio Code to watch large folders execute this on the host:
+
+       echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+   See: https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
+
+   This cannot be done during the Docker image build: https://github.com/cdr/code-server/issues/628#issuecomment-636526989
+   ")
 
 (defn install
   [params]
   (let [version (::version params
                            "1.60.2")]
-    (concat
-     [
-      ["wget"
-       (str "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v"
-            version
-            "/openvscode-server-v"
-            version
-            "-linux-x64.tar.gz")
-       "-O" "code-server.tar.gz"]
-      ["tar" "-xzf" "code-server.tar.gz"]
-      ["rm" "code-server.tar.gz"]
-      ["mv"
-       (str "openvscode-server-v"
-            version
-            "-linux-x64")
-       "/opt/openvscode"]
-      ["chown" "-R" "dev" "/opt/openvscode"]
-      ]
-     (when-let [max-user-watches (::max-user-watches params
-                                                     524288)]
-       ;; allows Visual Studio Code to watch large folders, see:
-       ;; https://code.visualstudio.com/docs/setup/linux#_visual-studio-code-is-unable-to-watch-for-file-changes-in-this-large-workspace-error-enospc
-       [["bash" "-c"
-         (str "echo "
-              "'fs.inotify.max_user_watches="
-              max-user-watches
-              "' >> /etc/sysctl.conf && sysctl -p")]])
-     )
+    [
+     ["wget"
+      (str "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v"
+           version
+           "/openvscode-server-v"
+           version
+           "-linux-x64.tar.gz")
+      "-O" "code-server.tar.gz"]
+     ["tar" "-xzf" "code-server.tar.gz"]
+     ["rm" "code-server.tar.gz"]
+     ["mv"
+      (str "openvscode-server-v"
+           version
+           "-linux-x64")
+      "/opt/openvscode"]
+     ["chown" "-R" "dev" "/opt/openvscode"]
+     ]
     ))
